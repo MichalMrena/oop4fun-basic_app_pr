15d3d6ea0df603394082d302588d019379276a37
Michal Mrena <mrena.michal@gmail.com>
2024-07-02 17:44:10 +0200

    Added conditional movement

--- Basic-Game-app/Beach.java
+++ Basic-Game-app/Beach.java
@@ -11,6 +11,7 @@ public class Beach extends World
     {    
         // Create a new world with 15x10 cells with a cell size of 60x60 pixels.
         super(15, 10, 60); 
-        this.addObject(new Police(), 0, 0);
+        this.addObject(new Police("w","s","a","d"), 0, 0);
+        //this.addObject(new Police("up","down","left","right"), 1, 1);
     }
 }
