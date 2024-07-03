0285ad42b36c13d883a6a961ea59b3457490d914
Michal Mrena <mrena.michal@gmail.com>
2024-07-03 11:40:14 +0200

    Added infinite game loop

--- Basic-Game-app/Beach.java
+++ Basic-Game-app/Beach.java
@@ -8,19 +8,13 @@ public class Beach extends World
     {    
         // Create a new world with 15x10 cells with a cell size of 60x60 pixels.
         super(15, 10, 60);
-        this.addObject(new Police("w","s","a","d"), 0, 0);
-        //this.addObject(new Police("up","down","left","right"), 1, 1);
-        this.addObject(
-            new Robber(),
-            Greenfoot.getRandomNumber(this.getWidth()),
-            Greenfoot.getRandomNumber(this.getHeight())
-        );
-        
-        this.counter = 500;
+        this.prepareTheBeach();
     }
     
     public void endTheGame(boolean win)
     {
+        this.removeObjects(this.getObjects(Actor.class));
+        
         int x = this.getWidth() / 2;
         int y = this.getHeight() / 2;
         String message;
@@ -30,6 +24,9 @@ public class Beach extends World
             message = "The robber escaped. You lost!";
         }
         this.showText(message, x, y);
+        Greenfoot.delay(40);
+        this.showText(null, x, y);
+        this.prepareTheBeach();
         Greenfoot.stop();
     }
     
@@ -39,5 +36,16 @@ public class Beach extends World
         if (this.counter == 0) {
             this.endTheGame(false);
         }
+    }
+    
+    private void prepareTheBeach()
+    {
+        this.addObject(new Police("w","s","a","d"), 0, 0);
+        this.addObject(
+            new Robber(),
+            Greenfoot.getRandomNumber(this.getWidth()),
+            Greenfoot.getRandomNumber(this.getHeight())
+        );        
+        this.counter = 500;       
     }
 }
